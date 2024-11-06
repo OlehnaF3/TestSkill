0f2e47c8b9b4d7003ea11206b87266943ba4603d
Oleg <sokol00076@gmail.com>
2024-11-06 22:09:48 +0500

    Обновление Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ namespace Prog
     {
         public static void Main(string[] args)
         {
-
+            Console.WriteLine("My name is Oleg");
+            Console.WriteLine("My age is 25");
+            Console.WriteLine("Do u hava a pet? False");
+            Console.WriteLine("My shoe size 42");
         }
     }
 
